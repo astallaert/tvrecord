66be4840509b6db04ba9267a824bdfe245bcea79
Adam Stallaert <astallaert@users.noreply.github.com>
2017-10-09 18:26:06 -0400

    Create checkvpn.sh

---
+++ checkvpn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -q --tries=10 --timeout=20 --spider http://google.com > /dev/null
+if [[ $? -eq 0 ]]; then
+        echo "Online"
+else
+        echo "Offline" && systemctl stop openvpn && systemctl start openvpn && $
+fi
