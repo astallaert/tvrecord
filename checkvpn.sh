67aff0d815da9a19442fad2c36e348b7338d8e15
Adam Stallaert <astallaert@users.noreply.github.com>
2017-10-09 18:31:46 -0400

    Update checkvpn.sh

--- checkvpn.sh
+++ checkvpn.sh
@@ -4,5 +4,5 @@ wget -q --tries=10 --timeout=20 --spider http://google.com > /dev/null
 if [[ $? -eq 0 ]]; then
         echo "Online"
 else
-        echo "Offline" && systemctl stop openvpn && systemctl start openvpn && $
+        echo "Offline" && systemctl stop openvpn && systemctl start openvpn && exit;
 fi
